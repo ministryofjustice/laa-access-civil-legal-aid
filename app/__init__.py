40891679eb4102ef0bb693cb98aff71a73e6c5c3
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2024-07-15 11:45:50 +0100

    Set profile sample to 20%

--- app/__init__.py
+++ app/__init__.py
@@ -25,7 +25,7 @@ if Config.SENTRY_DSN:
         # Set profiles_sample_rate to 1.0 to profile 100%
         # of sampled transactions.
         # We recommend adjusting this value in production.
-        profiles_sample_rate=1.0,
+        profiles_sample_rate=0.2,
     )
 
 def create_app(config_class=Config):
