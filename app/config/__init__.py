58d3542f9729eb6937e0cd3ad6025e4db3b29143
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-07-23 09:34:27 +0100

    Add environment parameter to Sentry (#16)

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 
 class Config(object):
+    ENVIRONMENT = os.environ.get("CLA_ENVIRONMENT", "production")
     CONTACT_EMAIL = os.environ.get("CONTACT_EMAIL", "")
     CONTACT_PHONE = os.environ.get("CONTACT_PHONE", "")
     DEPARTMENT_NAME = os.environ.get("DEPARTMENT_NAME", "MOJ Digital")
