47c34e50561470d46441db9ad5b8b15ef2c171d7
said-moj <45761276+said-moj@users.noreply.github.com>
2024-09-12 11:24:09 +0100

    Adding tests for maintenance mode pages

--- tests/unit_tests/test_pages.py
+++ tests/unit_tests/test_pages.py
@@ -1,3 +1,4 @@
+
 def test_set_locale(app, client):
     response = client.get("/locale/cy", headers={"referer": "http://localhost/privacy"})
     assert response.status_code == 302
@@ -9,3 +10,20 @@ def test_set_locale(app, client):
 def test_set_locale_invalid(app, client):
     response = client.get("/locale/de", headers={"referer": "http://localhost/privacy"})
     assert response.status_code == 404, f"Expecting 404 got {response.status_code}"
+
+
+def test_maintenance_page_on(app, client):
+    app.config["MAINTENANCE_MODE"] = True
+    response = client.get("/")
+    assert response.status_code == 302
+    assert response.headers["location"] == "/maintenance-mode"
+    response = client.get("/", follow_redirects=True)
+    assert response.status_code == 503
+    assert response.request.path == "/maintenance-mode"
+
+
+def test_maintenance_page_off(app, client):
+    app.config["MAINTENANCE_MODE"] = False
+    response = client.get("/maintenance-mode", follow_redirects=True)
+    assert response.status_code == 200
+    assert response.request.path == "/"
