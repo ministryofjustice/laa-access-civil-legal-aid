02cb6eee4d2d24612bd0762c0721491fe15e6cdd
said-moj <45761276+said-moj@users.noreply.github.com>
2024-10-08 09:59:05 +0100

    Use CLA_ENVIRONMENT  to toggle production flag

--- esbuild.config.js
+++ esbuild.config.js
@@ -2,7 +2,7 @@ const esbuild = require('esbuild');
 const { sassPlugin } = require('esbuild-sass-plugin');
 
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.CLA_ENVIRONMENT === 'production';
 
 async function build(should_watch) {
     const context = {
