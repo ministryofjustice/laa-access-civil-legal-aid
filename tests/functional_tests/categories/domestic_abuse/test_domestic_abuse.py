45f5d04cb03879ef0068e064658fcd932388357f
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-10-16 07:29:56 +0100

    Add comments
    Add comments

--- tests/functional_tests/categories/domestic_abuse/test_domestic_abuse.py
+++ tests/functional_tests/categories/domestic_abuse/test_domestic_abuse.py
@@ -4,6 +4,7 @@ import pytest
 
 @pytest.mark.usefixtures("live_server")
 def test_domestic_abuse_at_immediate_risk_of_harm(page: Page):
+    """If the user is at immediate risk of harm they should be immediately routed to the /contact page."""
     page.get_by_role("link", name="Domestic abuse").click()
     page.get_by_role("link", name="Help to protect you and your").click()
     page.get_by_label("Yes").check()
@@ -15,6 +16,7 @@ def test_domestic_abuse_at_immediate_risk_of_harm(page: Page):
 
 @pytest.mark.usefixtures("live_server")
 def test_domestic_abuse_not_at_immediate_risk_of_harm(page: Page):
+    """This is where the current prototype journey ends, there are no onward pages yet."""
     page.get_by_role("link", name="Domestic abuse").click()
     page.get_by_role("link", name="Help to protect you and your").click()
     page.get_by_label("No").check()
