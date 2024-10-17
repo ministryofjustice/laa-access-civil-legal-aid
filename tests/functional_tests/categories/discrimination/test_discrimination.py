7bfe086b10e0a282a24b5000113ca17279fad9ee
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-10-17 14:38:38 +0100

    Add functional tests for discrimination journey

---
+++ tests/functional_tests/categories/discrimination/test_discrimination.py
@@ -0,0 +1,14 @@
+from playwright.sync_api import Page, expect
+import pytest
+
+
+@pytest.mark.usefixtures("live_server")
+def test_discrimination(page: Page):
+    page.get_by_role("link", name="Discrimination").click()
+    page.get_by_label("Work").check()
+    page.get_by_role("button", name="Continue").click()
+    page.get_by_label("Disability").check()
+    page.get_by_role("button", name="Continue").click()
+    expect(
+        page.get_by_role("link", name="Check if you can get legal aid")
+    ).to_be_visible()
