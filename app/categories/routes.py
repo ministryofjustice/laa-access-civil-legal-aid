b60b9db0962a6d19e6cfff57a4d0b99e3615d0d0
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2024-10-17 13:56:34 +0100

    Add more problems route

--- app/categories/routes.py
+++ app/categories/routes.py
@@ -5,3 +5,7 @@ from flask import render_template
 @bp.route("/")
 def index():
     return render_template("index.html")
+
+@bp.route("/more-problems")
+def more_problems():
+    return render_template("more-problems.html")
