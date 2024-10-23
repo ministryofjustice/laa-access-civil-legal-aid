8f95655dd61bb166b9bbda78e786931a7ff947cc
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-10-23 10:01:24 +0100

    User journeys should end with 'End of prototype'

--- app/categories/discrimination/routes.py
+++ app/categories/discrimination/routes.py
@@ -1,5 +1,5 @@
 from app.categories.discrimination import bp
-from flask import render_template, url_for, flash, redirect, current_app
+from flask import render_template, url_for, flash, redirect
 from app.categories.discrimination.forms import (
     DiscriminationWhereForm,
     DiscriminationWhyForm,
@@ -14,7 +14,9 @@ def where_did_the_discrimination_happen():
     items = get_items_with_divisor(form.question.choices)
 
     if form.validate_on_submit():
-        flash("End of prototype")
+        return redirect(
+            url_for("categories.discrimination.why_were_you_treated_differently")
+        )
 
     return render_template(
         "categories/question-page.html",
@@ -31,10 +33,6 @@ def why_were_you_treated_differently(where):
     items = get_items_with_divisor(form.question.choices)
 
     if form.validate_on_submit():
-        if form.question.data == "disability":
-            return redirect(
-                f"{current_app.config['CLA_PUBLIC_URL']}/discrimination_means"
-            )
         flash("End of prototype")
 
     return render_template(
