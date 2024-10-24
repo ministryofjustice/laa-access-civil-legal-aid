d6a941f53465b0807d08e96d820bec9271b2a847
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-10-24 13:50:23 +0100

    Update clinical negligence redirect

--- app/categories/traversal.py
+++ app/categories/traversal.py
@@ -12,7 +12,7 @@ class CategoryTraversal:
         "discrimination": DiscriminationWhereForm,
         "domestic-abuse": DomesticAbuseTraversal,
         "clinical-negligence": redirect(
-            "http://127.0.0.1:5000/scope/refer/legal-adviser?category=clinneg"
+            "https://checklegalaid.service.gov.uk/scope/refer/legal-adviser?category=clinneg"
         ),
     }
 
