0fbf065b833ae56bc0f2af2b4fbb32f8f4b480e5
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2024-11-25 15:18:39 +0000

    Add secondary category option

--- app/find_a_legal_advisor/routes.py
+++ app/find_a_legal_advisor/routes.py
@@ -11,22 +11,36 @@ def search():
     form = FindLegalAdviserForm(request.args)
 
     category: str | None = request.args.get("category", default=None, type=str)
+    secondary_category: str | None = request.args.get(
+        "secondary_category", default=None, type=str
+    )
 
     if "postcode" in request.args and form.validate():
         postcode: str | None = form.postcode.data
         page_num: int = request.args.get("page", 1, type=int)
         postcode_region: str = form.postcode_region
-        return result_page(postcode, category, page_num, postcode_region)
+        return result_page(
+            postcode, category, secondary_category, page_num, postcode_region
+        )
 
     return render_template(
-        "find_a_legal_adviser/search.html", form=form, category=category
+        "find_a_legal_adviser/search.html",
+        form=form,
+        category=category,
+        secondary_category=secondary_category,
     )
 
 
 def result_page(
-    postcode: str, category: str = None, page_num: int = 1, postcode_region: str = None
+    postcode: str,
+    category: str = None,
+    secondary_category: str = None,
+    page_num: int = 1,
+    postcode_region: str = None,
 ):
-    results = laalaa_search(postcode=postcode, category=category, page=page_num)
+    results = laalaa_search(
+        postcode=postcode, categories=[category, secondary_category], page=page_num
+    )
 
     pagination_data = get_pagination_data(
         results, postcode=postcode, category=category, current_page_num=page_num
@@ -36,6 +50,7 @@ def result_page(
         "find_a_legal_adviser/results.html",
         data=results,
         category=category,
+        secondary_category=secondary_category,
         pagination_data=pagination_data,
         postcode_region=postcode_region,
     )
