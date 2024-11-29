06acaec3924f6e3a910ac2a75bb2cc8903118ffb
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-11-29 17:31:58 +0000

    Update domestic abuse landing page to use the new structure

--- app/categories/domestic_abuse/urls.py
+++ app/categories/domestic_abuse/urls.py
@@ -2,9 +2,22 @@ from app.categories.domestic_abuse import bp
 from app.categories.domestic_abuse.forms import AreYouAtRiskOfHarmForm
 from app.categories.views import QuestionPage, CategoryLandingPage
 
+
+class DomesticAbuseLandingPage(CategoryLandingPage):
+    template = "categories/domestic-abuse.html"
+
+    question_title = "Domestic Abuse"
+
+    category = "Domestic Abuse"
+
+    routing_map = {
+        "protect_you_and_your_children": "categories.domestic_abuse.are_you_at_risk_of_harm",
+    }
+
+
 bp.add_url_rule(
     "/domestic-abuse/",
-    view_func=CategoryLandingPage.as_view("landing", "categories/domestic-abuse.html"),
+    view_func=DomesticAbuseLandingPage.as_view("landing", bp),
 )
 bp.add_url_rule(
     "/domestic-abuse/are-you-at-risk-of-harm",
