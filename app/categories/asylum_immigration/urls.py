d78ed78176c27a5746ddb68ee67f8ae8c588ff7e
said-moj <45761276+said-moj@users.noreply.github.com>
2024-12-02 13:28:01 +0000

    Add Asylum and immigration category landing page

---
+++ app/categories/asylum_immigration/urls.py
@@ -0,0 +1,31 @@
+from app.categories.asylum_immigration import bp
+from app.categories.views import CategoryLandingPage
+
+
+FALA_REDIRECT = {
+    "endpoint": "find-a-legal-adviser.search",
+    "category": "immas",
+}
+
+
+class AsylumAndImmigrationLandingPage(CategoryLandingPage):
+    question_title = "Asylum and immigration"
+    category = "asylum_immigration"
+
+    routing_map = {
+        "apply": FALA_REDIRECT,
+        "housing": "categories.housing.homelessness",
+        "domestic_abuse": FALA_REDIRECT,
+        "detained": FALA_REDIRECT,
+        "modern_slavery": FALA_REDIRECT,
+    }
+
+
+bp.add_url_rule(
+    "/asylum-and-immigration",
+    view_func=AsylumAndImmigrationLandingPage.as_view(
+        "landing", template="categories/asylum-and-immigration/landing.html"
+    ),
+)
+
+AsylumAndImmigrationLandingPage.register_routes(blueprint=bp)
---
+++ tests/functional_tests/categories/asylum_immigration/test_asylum_immigration.py
@@ -0,0 +1,35 @@
+from playwright.sync_api import Page, expect
+import pytest
+
+in_scope_page_heading = "Legal aid is available for this type of problem"
+fala_page_heading = "Find a legal adviser"
+
+ROUTING = [
+    {"link_text": "Applying for asylum", "next_page_heading": fala_page_heading},
+    {
+        "link_text": "Housing and homelessness",
+        "next_page_heading": in_scope_page_heading,
+    },
+    {
+        "link_text": "Stay in the UK if you experienced domestic abuse",
+        "next_page_heading": fala_page_heading,
+    },
+    {
+        "link_text": "Help if you’re being detained",
+        "next_page_heading": fala_page_heading,
+    },
+    {
+        "link_text": "Trafficking, modern slavery",
+        "next_page_heading": fala_page_heading,
+    },
+    {"link_text": "Next steps to get help", "next_page_heading": "Referral page"},
+]
+
+
+@pytest.mark.usefixtures("live_server")
+class TestAsylumAndImmigrationLandingPage:
+    @pytest.mark.parametrize("routing", ROUTING)
+    def test_onward_routing(self, page: Page, routing: dict):
+        page.get_by_role("link", name="Asylum and immigration").click()
+        page.get_by_role("link", name=routing["link_text"]).click()
+        expect(page.get_by_text(routing["next_page_heading"])).to_be_visible()
