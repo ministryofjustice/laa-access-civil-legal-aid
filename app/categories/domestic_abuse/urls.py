5979eedc84a3dae8b971419d9544a0b403b5dfd6
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2024-12-02 13:18:09 +0000

    Add domestic abuse path and tests

--- app/categories/domestic_abuse/urls.py
+++ app/categories/domestic_abuse/urls.py
@@ -10,13 +10,20 @@ class DomesticAbuseLandingPage(CategoryLandingPage):
 
     routing_map = {
         "protect_you_and_your_children": "categories.domestic_abuse.are_you_at_risk_of_harm",
+        "leaving_an_abusive_relationship": "categories.domestic_abuse.are_you_at_risk_of_harm",
+        "problems_with_ex_partner": "categories.domestic_abuse.are_you_at_risk_of_harm",
+        "problems_with_neighbours": "categories.results.contact",
+        "housing_homelessness_losing_home": "categories.housing.landing",
+        "forced_marriage": "categories.domestic_abuse.are_you_at_risk_of_harm",
+        "fgm": "categories.domestic_abuse.are_you_at_risk_of_harm",
+        "other": "categories.results.refer",
     }
 
 
 bp.add_url_rule(
     "/domestic-abuse/",
     view_func=DomesticAbuseLandingPage.as_view(
-        "landing", template="categories/domestic-abuse.html"
+        "landing", template="categories/domestic_abuse/landing.html"
     ),
 )
 bp.add_url_rule(
