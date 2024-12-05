4999f47317bd0ccfd79a694a152b4881be0b4fea
said-moj <45761276+said-moj@users.noreply.github.com>
2024-12-05 13:42:55 +0000

    Add benefits appeal category

--- app/categories/__init__.py
+++ app/categories/__init__.py
@@ -5,6 +5,7 @@ from .asylum_immigration import bp as asylum_immigration_bp
 from .housing import bp as housing_bp
 from .mental_capacity import bp as mental_capacity_bp
 from .community_care import bp as community_care_bp
+from .benefits import bp as benefits_bp
 from .results import bp as results_bp
 
 bp = Blueprint("categories", __name__)
@@ -15,5 +16,6 @@ bp.register_blueprint(mental_capacity_bp)
 bp.register_blueprint(results_bp)
 bp.register_blueprint(asylum_immigration_bp)
 bp.register_blueprint(community_care_bp)
+bp.register_blueprint(benefits_bp)
 
 from app.categories import urls  # noqa: E402,F401
