42adc86f30b1ebf166ddf56d841971e76ecf1055
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-12-27 13:33:58 +0000

    Improve help organisation fetching

--- app/categories/results/help_organisations.py
+++ app/categories/results/help_organisations.py
@@ -1,25 +1,15 @@
-from urllib.parse import urlencode
 import requests
 from flask import current_app
 
 
-def kwargs_to_urlparams(**kwargs):
-    kwargs = dict(filter(lambda kwarg: kwarg[1], kwargs.items()))
-    return urlencode(kwargs, True)
-
-
-def backend_url(endpoint: str = "organisation", **kwargs):
-    backend_api_host = current_app.config["BACKEND_URL"]
-    params = kwargs_to_urlparams(**kwargs)
-    return f"{backend_api_host}/checker/api/v1/{endpoint}/?{params}"
-
-
 def get_help_organisations(category: str, **kwargs):
-    if isinstance(category, str):
-        category = (
-            category.title()
-        )  # CLA Backend requires the category name to be title case
+    if not isinstance(category, str):
+        return []
+    kwargs["article_category__name"] = (
+        category.title()
+    )  # CLA Backend requires the category name to be title case
+    hostname = current_app.config["BACKEND_URL"]
     response = requests.get(
-        backend_url(article_category__name=category, **kwargs)
+        url=f"{hostname}/checker/api/v1/organisation", params=kwargs
     ).json()
     return response["results"]
