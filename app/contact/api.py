a1f440ee9e322512655c3658d5d33931456b5247
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2024-12-31 14:39:17 +0000

    Update url for payload

--- app/contact/api.py
+++ app/contact/api.py
@@ -6,8 +6,6 @@ def post_reasons_for_contacting(form=None, payload={}):
     hostname = current_app.config["CLA_BACKEND_URL"]
     payload = form.api_payload() if form else payload
     response = requests.post(
-        url=f"{hostname}/checker/api/v1/reasons-for-contacting", json=payload
+        url=f"{hostname}/checker/api/v1/reasons_for_contacting/", json=payload
     )
-    # Ensure exceptions are raised for HTTP errors
-    response.raise_for_status()
-    return response.json()
+    return response
