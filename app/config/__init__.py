190e0be0c380c69f4a8dda8b50379760a8e04124
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-01-02 14:42:32 +0000

    Revert to local

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -26,7 +26,4 @@ class Config(object):
         "https://laa-legal-adviser-api-production.cloud-platform.service.justice.gov.uk",
     )
     POSTCODES_IO_URL = os.environ.get("POSTCODES_IO_URL", "https://api.postcodes.io")
-    CLA_BACKEND_URL = os.environ.get(
-        "CLA_BACKEND_URL",
-        "http://cla-backend-app.laa-cla-backend-production.svc.cluster.local",
-    )
+    CLA_BACKEND_URL = os.environ.get("CLA_BACKEND_URL", "http://127.0.0.1:8010")
