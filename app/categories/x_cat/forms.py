eb056540addd4871772f2e31ccedf1c3e6c16b9a
Ben Millar <ben.millar@digital.justice.gov.uk>
2025-01-03 12:01:31 +0000

    Remove duplicate form

--- app/categories/x_cat/forms.py
+++ app/categories/x_cat/forms.py
@@ -5,35 +5,6 @@ from app.categories.forms import QuestionForm
 from flask_babel import lazy_gettext as _
 
 
-class AntiSocialBehaviourForm(QuestionForm):
-    title = _("Were you accused by a landlord or the council?")
-    category = "Anti-social behaviour and gangs"
-    next_step_mapping = {
-        "yes": {
-            "endpoint": "categories.results.in_scope",
-        },
-        "no": {
-            "endpoint": "find-a-legal-adviser.search",
-        },
-    }
-
-    question = RadioField(
-        title,
-        widget=CategoryRadioInput(show_divider=False, is_inline=True),
-        validators=[
-            InputRequired(
-                message=_(
-                    "Select ‘Yes’ if you were accused by a landlord or the council"
-                )
-            )
-        ],
-        choices=[
-            ("yes", _("Yes")),
-            ("no", _("No")),
-        ],
-    )
-
-
 class AreYouUnder18Form(QuestionForm):
     title = _("Are you under 18?")
 
