3b83ad9b53bb7aa35c70542bf0e58d71c1bdeba7
Ben Millar <ben.millar@digital.justice.gov.uk>
2025-01-03 12:00:21 +0000

    LGA 3372: Are you under 18 (#88)
    * Add are you under 18 page
    
    * Update child in care question to use caption form
    
    * Update 'Is this to do with police, prisons or detention centres?' question to use caption question format
    
    * Update functional tests
    
    * Update discrimination flow

--- app/categories/send/forms.py
+++ app/categories/send/forms.py
@@ -1,5 +1,15 @@
 from app.categories.forms import ChildInCareQuestionForm
+from app.categories.x_cat.forms import AreYouUnder18Form
 
 
 class SendChildInCareQuestionForm(ChildInCareQuestionForm):
+    category = "SEND"
+
+    next_step_mapping = {
+        "yes": "categories.send.age",
+        "no": "categories.send.age",
+    }
+
+
+class SendAreYouUnder18Form(AreYouUnder18Form):
     category = "SEND"
