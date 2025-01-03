df8a76106619d8184bd735ecbe45bd7a741f1166
Ben Millar <ben.millar@digital.justice.gov.uk>
2025-01-03 12:05:23 +0000

    Remove duplicate URL entry

--- app/categories/send/urls.py
+++ app/categories/send/urls.py
@@ -1,5 +1,5 @@
 from app.categories.send import bp
-from app.categories.send.forms import SendChildInCareQuestionForm, SendAreYouUnder18Form
+from app.categories.send.forms import SendChildInCareQuestionForm
 from app.categories.views import CategoryLandingPage, QuestionPage
 
 
@@ -29,14 +29,6 @@ bp.add_url_rule(
     view_func=QuestionPage.as_view(
         "child_in_care",
         form_class=SendChildInCareQuestionForm,
-        template="categories/question-page-caption.html",
-    ),
-)
-bp.add_url_rule(
-    "/send/age",
-    view_func=QuestionPage.as_view(
-        "age",
-        form_class=SendAreYouUnder18Form,
         template="categories/question-page-caption.html",
     ),
 )
