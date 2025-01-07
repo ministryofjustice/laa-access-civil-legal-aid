4138e1aef67a696d6f514e18fd710778a0cd8406
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-01-07 13:24:14 +0000

    Refactor

--- app/main/api.py
+++ app/main/api.py
@@ -10,7 +10,6 @@ def kwargs_to_urlparams(**kwargs):
 def backend_url(endpoint: str, **kwargs):
     backend_host_url = current_app.config["CLA_BACKEND_URL"]
     api_route = "/checker/api/v1/"
-    endpoint = endpoint
     params = kwargs_to_urlparams(**kwargs)
     if params:
         return f"{backend_host_url}{api_route}{endpoint}?{params}"
