1598b545c418018b5c64ddaa7d394ae40532cbbb
Ben Millar <ben.millar@digital.justice.gov.uk>
2025-01-07 16:52:33 +0000

    Fix typo

--- app/categories/discrimination/forms.py
+++ app/categories/discrimination/forms.py
@@ -3,7 +3,7 @@ from app.categories.widgets import CategoryCheckboxInput
 from app.categories.forms import QuestionForm
 from wtforms.validators import InputRequired
 from app.categories.x_cat.forms import AreYouUnder18Form
-from app.categories.categories import Category
+from app.categories.constants import Category
 
 
 class DiscriminationQuestionForm(QuestionForm):
