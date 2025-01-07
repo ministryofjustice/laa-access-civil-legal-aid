b4194e4d031c7119036bc4a61444939ab86df3fc
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-01-07 13:09:18 +0000

    Add params to backend url

--- app/main/api.py
+++ app/main/api.py
@@ -1,8 +1,17 @@
 from flask import current_app
+from urllib.parse import urlencode
 
 
-def backend_url(endpoint: str):
+def kwargs_to_urlparams(**kwargs):
+    kwargs = dict(filter(lambda kwarg: kwarg[1], kwargs.items()))
+    return urlencode(kwargs, True)
+
+
+def backend_url(endpoint: str, **kwargs):
     backend_host_url = current_app.config["CLA_BACKEND_URL"]
     api_route = "/checker/api/v1/"
     endpoint = endpoint
+    params = kwargs_to_urlparams(**kwargs)
+    if params:
+        return f"{backend_host_url}{api_route}{endpoint}?{params}"
     return f"{backend_host_url}{api_route}{endpoint}"
