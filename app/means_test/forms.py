1822ace15e5080798318078cd9d1e59b97db3164
Ben Millar <ben.millar@digital.justice.gov.uk>
2025-01-10 16:14:02 +0000

    Add form validation

--- app/means_test/forms.py
+++ app/means_test/forms.py
@@ -1,7 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import RadioField, IntegerField
-from govuk_frontend_wtf.wtforms_widgets import GovTextInput
+from govuk_frontend_wtf.wtforms_widgets import GovTextInput, GovSubmitInput
+from wtforms.fields.simple import SubmitField
+from wtforms.validators import InputRequired, NumberRange
+
+from app.means_test.validators import ValidateIf
 from app.means_test.widgets import MeansTestRadioInput
+from flask_babel import gettext as _
 
 
 class AboutYouForm(FlaskForm):
@@ -12,74 +17,128 @@ class AboutYouForm(FlaskForm):
         choices=[("yes", "Yes"), ("no", "No")],
         widget=MeansTestRadioInput(),
         description="Your husband, wife, civil partner (unless you have permanently separated) or someone you live with as if you're married",
+        validators=[InputRequired(message=_("Tell us whether you have a partner"))],
     )
 
     are_you_in_a_dispute = RadioField(
         "Are you in a dispute with your partner?",
         choices=[("yes", "Yes"), ("no", "No")],
         widget=MeansTestRadioInput(),
+        validators=[
+            ValidateIf("partner", "yes"),
+            InputRequired(
+                message=_("Tell us whether you’re in dispute with your partner")
+            ),
+        ],
     )
 
-    do_you_receive_benefits = RadioField(
+    on_benefits = RadioField(
         "Do you receive any benefits (including Child Benefit)?",
         choices=[("yes", "Yes"), ("no", "No")],
         widget=MeansTestRadioInput(),
         description="Being on some benefits can help you qualify for legal aid",
+        validators=[InputRequired(message=_("Tell us whether you receive benefits"))],
     )
 
-    children_under_16 = RadioField(
+    have_children = RadioField(
         "Do you have any children aged 15 or under?",
         choices=[("yes", "Yes"), ("no", "No")],
         widget=MeansTestRadioInput(),
         description="Don't include any children who don't live with you",
+        validators=[
+            InputRequired(
+                message=_("Tell us whether you have any children aged 15 or under")
+            )
+        ],
     )
 
-    how_many_children_under_16 = IntegerField("How many?", widget=GovTextInput())
+    num_children = IntegerField(
+        "How many?",
+        widget=GovTextInput(),
+        validators=[
+            ValidateIf("have_children", "yes"),
+            InputRequired(
+                message=_("Tell us how many children you have aged 15 or under")
+            ),
+            NumberRange(min=1, max=50, message=_("Enter a number between 1 and 50")),
+        ],
+    )
 
-    dependants_over_16 = RadioField(
+    have_dependents = RadioField(
         "Do you have any dependants aged 16 or over?",
         choices=[("yes", "Yes"), ("no", "No")],
         widget=MeansTestRadioInput(),
         description="People who you live with and support financially. This could be a young person for whom you get Child Benefit",
+        validators=[
+            InputRequired(
+                message=_("Tell us whether you have any dependants aged 16 or over")
+            )
+        ],
     )
 
-    how_many_dependents_over_16 = IntegerField("How many?", widget=GovTextInput())
+    num_dependents = IntegerField(
+        "How many?",
+        widget=GovTextInput(),
+        validators=[
+            ValidateIf("have_dependents", "yes"),
+            InputRequired(_("Tell us how many dependants you have aged 16 or over")),
+            NumberRange(min=1, max=50, message=_("Enter a number between 1 and 50")),
+        ],
+    )
 
     own_property = RadioField(
         "Do you own any property?",
         choices=[("yes", "Yes"), ("no", "No")],
         widget=MeansTestRadioInput(),
         description="For example, a house, static caravan or flat",
+        validators=[InputRequired(message=_("Tell us if you own any properties"))],
     )
 
-    employed = RadioField(
+    is_employed = RadioField(
         "Are you employed?",
         choices=[("yes", "Yes"), ("no", "No")],
         widget=MeansTestRadioInput(),
         description="This means working as an employee - you may be both employed and self-employed",
+        validators=[InputRequired(message=_("Tell us if you are employed"))],
     )
 
-    self_employed = RadioField(
+    is_self_employed = RadioField(
         "Are you self-employed?",
         choices=[("yes", "Yes"), ("no", "No")],
         widget=MeansTestRadioInput(),
         description="This means working for yourself - you may be both employed and self-employed",
+        validators=[InputRequired(message=_("Tell us if you are self-employed"))],
     )
 
-    over_60 = RadioField(
+    aged_60_or_over = RadioField(
         "Are you or your partner (if you have one) aged 60 or over?",
         choices=[("yes", "Yes"), ("no", "No")],
         widget=MeansTestRadioInput(),
+        validators=[
+            InputRequired(
+                message=_("Tell us if you or your partner are aged 60 or over")
+            )
+        ],
     )
 
-    savings_or_investments = RadioField(
+    have_savings = RadioField(
         "Do you have any savings or investments?",
         choices=[("yes", "Yes"), ("no", "No")],
         widget=MeansTestRadioInput(),
+        validators=[
+            InputRequired(message=_("Tell us whether you have savings or investments"))
+        ],
     )
 
-    valuable_items = RadioField(
+    have_valuables = RadioField(
         "Do you have any valuable items worth over £500 each?",
         choices=[("yes", "Yes"), ("no", "No")],
         widget=MeansTestRadioInput(),
+        validators=[
+            InputRequired(
+                message=_("Tell us if you have any valuable items worth over £500 each")
+            )
+        ],
     )
+
+    submit = SubmitField(_("Continue"), widget=GovSubmitInput())
--- app/means_test/routes.py
+++ app/means_test/routes.py
@@ -1,9 +1,13 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app.means_test import bp
 from app.means_test.forms import AboutYouForm
 
 
-@bp.get("/about-you")
+@bp.route("/about-you", methods=["GET", "POST"])
 def about_you():
     form = AboutYouForm()
+
+    if form.validate_on_submit():
+        return redirect(url_for("means_test.about-your-income"))
+
     return render_template("means_test/about-you.html", form=form)
