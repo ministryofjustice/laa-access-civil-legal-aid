c432d37a1697be28a75669ec93c50d353802c38a
Ben Millar <ben.millar@digital.justice.gov.uk>
2025-01-13 15:34:36 +0000

    Send About You payload to cla_backend

--- app/means_test/api.py
+++ app/means_test/api.py
@@ -2,23 +2,16 @@ from app.api import cla_backend
 from flask import session
 
 
-def update_means_test(payload):
+def update_means_test(form_data):
     means_test_endpoint = "checker/api/v1/eligibility_check/"
 
     ec_reference = session.get("reference")
 
     if ec_reference:
-        response = cla_backend.patch(
-            f"{means_test_endpoint}{ec_reference}", json=payload
-        )
-        return response
+        print(form_data)
+        form_data["reference"] = ec_reference
+        cla_backend.patch(f"{means_test_endpoint}/{ec_reference}", json=form_data)
     else:
-        response = cla_backend.post(means_test_endpoint, json=payload)
+        print(form_data)
+        response = cla_backend.post(means_test_endpoint, json=form_data)
         session["reference"] = response["reference"]
-        return response
-
-
-def is_eligible(reference):
-    means_test_endpoint = "checker/api/v1/eligibility_check/"
-    response = cla_backend.post(f"{means_test_endpoint}{reference}/is_eligible/")
-    return response["is_eligible"]
--- app/means_test/forms.py
+++ app/means_test/forms.py
@@ -8,13 +8,16 @@ from app.means_test.validators import ValidateIf
 from app.means_test.widgets import MeansTestRadioInput
 from flask_babel import gettext as _
 
+YES = "1"
+NO = "0"
+
 
 class AboutYouForm(FlaskForm):
     title = "About you"
 
-    partner = RadioField(
+    has_partner = RadioField(
         "Do you have a partner?",
-        choices=[("yes", "Yes"), ("no", "No")],
+        choices=[(YES, "Yes"), (NO, "No")],
         widget=MeansTestRadioInput(),
         description="Your husband, wife, civil partner (unless you have permanently separated) or someone you live with as if you're married",
         validators=[InputRequired(message=_("Tell us whether you have a partner"))],
@@ -22,10 +25,10 @@ class AboutYouForm(FlaskForm):
 
     are_you_in_a_dispute = RadioField(
         "Are you in a dispute with your partner?",
-        choices=[("yes", "Yes"), ("no", "No")],
+        choices=[(YES, "Yes"), (NO, "No")],
         widget=MeansTestRadioInput(),
         validators=[
-            ValidateIf("partner", "yes"),
+            ValidateIf("has_partner", YES),
             InputRequired(
                 message=_("Tell us whether you’re in dispute with your partner")
             ),
@@ -34,7 +37,7 @@ class AboutYouForm(FlaskForm):
 
     on_benefits = RadioField(
         "Do you receive any benefits (including Child Benefit)?",
-        choices=[("yes", "Yes"), ("no", "No")],
+        choices=[(YES, "Yes"), (NO, "No")],
         widget=MeansTestRadioInput(),
         description="Being on some benefits can help you qualify for legal aid",
         validators=[InputRequired(message=_("Tell us whether you receive benefits"))],
@@ -42,7 +45,7 @@ class AboutYouForm(FlaskForm):
 
     have_children = RadioField(
         "Do you have any children aged 15 or under?",
-        choices=[("yes", "Yes"), ("no", "No")],
+        choices=[(YES, "Yes"), (NO, "No")],
         widget=MeansTestRadioInput(),
         description="Don't include any children who don't live with you",
         validators=[
@@ -56,7 +59,7 @@ class AboutYouForm(FlaskForm):
         "How many?",
         widget=GovTextInput(),
         validators=[
-            ValidateIf("have_children", "yes"),
+            ValidateIf("have_children", YES),
             InputRequired(
                 message=_("Tell us how many children you have aged 15 or under")
             ),
@@ -66,7 +69,7 @@ class AboutYouForm(FlaskForm):
 
     have_dependents = RadioField(
         "Do you have any dependants aged 16 or over?",
-        choices=[("yes", "Yes"), ("no", "No")],
+        choices=[(YES, "Yes"), (NO, "No")],
         widget=MeansTestRadioInput(),
         description="People who you live with and support financially. This could be a young person for whom you get Child Benefit",
         validators=[
@@ -80,7 +83,7 @@ class AboutYouForm(FlaskForm):
         "How many?",
         widget=GovTextInput(),
         validators=[
-            ValidateIf("have_dependents", "yes"),
+            ValidateIf("have_dependents", YES),
             InputRequired(_("Tell us how many dependants you have aged 16 or over")),
             NumberRange(min=1, max=50, message=_("Enter a number between 1 and 50")),
         ],
@@ -88,7 +91,7 @@ class AboutYouForm(FlaskForm):
 
     own_property = RadioField(
         "Do you own any property?",
-        choices=[("yes", "Yes"), ("no", "No")],
+        choices=[(YES, "Yes"), (NO, "No")],
         widget=MeansTestRadioInput(),
         description="For example, a house, static caravan or flat",
         validators=[InputRequired(message=_("Tell us if you own any properties"))],
@@ -96,23 +99,49 @@ class AboutYouForm(FlaskForm):
 
     is_employed = RadioField(
         "Are you employed?",
-        choices=[("yes", "Yes"), ("no", "No")],
+        choices=[(YES, "Yes"), (NO, "No")],
         widget=MeansTestRadioInput(),
         description="This means working as an employee - you may be both employed and self-employed",
         validators=[InputRequired(message=_("Tell us if you are employed"))],
     )
 
+    partner_is_employed = RadioField(
+        _("Is your partner employed?"),
+        description=_(
+            "This means working as an employee - your partner may be both employed and self-employed"
+        ),
+        choices=[(YES, "Yes"), (NO, "No")],
+        validators=[
+            ValidateIf("are_you_in_a_dispute", NO),
+            InputRequired(message=_("Tell us whether your partner is employed")),
+        ],
+        widget=MeansTestRadioInput(),
+    )
+
     is_self_employed = RadioField(
         "Are you self-employed?",
-        choices=[("yes", "Yes"), ("no", "No")],
+        choices=[(YES, "Yes"), (NO, "No")],
         widget=MeansTestRadioInput(),
         description="This means working for yourself - you may be both employed and self-employed",
         validators=[InputRequired(message=_("Tell us if you are self-employed"))],
     )
 
+    partner_is_self_employed = RadioField(
+        _("Is your partner self-employed?"),
+        description=_(
+            "This means working for yourself - your partner may be both employed and self-employed"
+        ),
+        choices=[(YES, "Yes"), (NO, "No")],
+        validators=[
+            ValidateIf("are_you_in_a_dispute", NO),
+            InputRequired(message=_("Tell us whether your partner is self-employed")),
+        ],
+        widget=MeansTestRadioInput(),
+    )
+
     aged_60_or_over = RadioField(
         "Are you or your partner (if you have one) aged 60 or over?",
-        choices=[("yes", "Yes"), ("no", "No")],
+        choices=[(YES, "Yes"), (NO, "No")],
         widget=MeansTestRadioInput(),
         validators=[
             InputRequired(
@@ -123,7 +152,7 @@ class AboutYouForm(FlaskForm):
 
     have_savings = RadioField(
         "Do you have any savings or investments?",
-        choices=[("yes", "Yes"), ("no", "No")],
+        choices=[(YES, "Yes"), (NO, "No")],
         widget=MeansTestRadioInput(),
         validators=[
             InputRequired(message=_("Tell us whether you have savings or investments"))
@@ -132,7 +161,7 @@ class AboutYouForm(FlaskForm):
 
     have_valuables = RadioField(
         "Do you have any valuable items worth over £500 each?",
-        choices=[("yes", "Yes"), ("no", "No")],
+        choices=[(YES, "Yes"), (NO, "No")],
         widget=MeansTestRadioInput(),
         validators=[
             InputRequired(
@@ -142,3 +171,264 @@ class AboutYouForm(FlaskForm):
     )
 
     submit = SubmitField(_("Continue"), widget=GovSubmitInput())
+
+    def payload_2(self):
+        return {
+            "on_passported_benefits": "0",
+            "category": "u" "debt",
+            "is_you_or_your_partner_over_60": "u" "0",
+            "has_partner": "0",
+            "property_set": [],
+            "specific_benefits": {},
+            "dependants_old": 0,
+            "you": {
+                "savings": {
+                    "credit_balance": 0,
+                    "investment_balance": 0,
+                    "asset_balance": 0,
+                    "bank_balance": 0,
+                },
+                "deductions": {
+                    "income_tax": {
+                        "per_interval_value": 0,
+                        "interval_period": "per_month",
+                    },
+                    "mortgage": {
+                        "per_interval_value": 0,
+                        "interval_period": "per_month",
+                    },
+                    "childcare": {
+                        "per_interval_value": 0,
+                        "interval_period": "per_month",
+                    },
+                    "rent": {
+                        "per_interval_value": 0,
+                        "interval_period": "u" "per_week",
+                    },
+                    "maintenance": {
+                        "per_interval_value": 0,
+                        "interval_period": "u" "per_week",
+                    },
+                    "criminal_legalaid_contributions": 0,
+                    "national_insurance": {
+                        "per_interval_value": 0,
+                        "interval_period": "per_month",
+                    },
+                },
+                "income": {
+                    "self_employment_drawings": {
+                        "per_interval_value": 0,
+                        "interval_period": "per_month",
+                    },
+                    "benefits": {
+                        "per_interval_value": 0,
+                        "interval_period": "per_month",
+                    },
+                    "maintenance_received": {
+                        "per_interval_value": 0,
+                        "interval_period": "u" "per_week",
+                    },
+                    "self_employed": "0",
+                    "tax_credits": {
+                        "per_interval_value": 0,
+                        "interval_period": "per_month",
+                    },
+                    "earnings": {
+                        "per_interval_value": 0,
+                        "interval_period": "per_month",
+                    },
+                    "child_benefits": {
+                        "per_interval_value": 0,
+                        "interval_period": "per_month",
+                    },
+                    "other_income": {
+                        "per_interval_value": 0,
+                        "interval_period": "u" "per_week",
+                    },
+                    "pension": {
+                        "per_interval_value": 0,
+                        "interval_period": "u" "per_week",
+                    },
+                },
+            },
+            "dependants_young": 0,
+            "on_nass_benefits": "0",
+        }
+
+    def payload(self):
+        payload = {
+            "has_partner": YES
+            if self.has_partner.data == YES and not self.are_you_in_a_dispute.data == NO
+            else NO,
+            "is_you_or_your_partner_over_60": "1",
+            "dependants_young": self.num_children.data
+            if self.have_children.data == YES
+            else 0,
+            "dependants_old": self.num_dependents.data
+            if self.have_dependents.data == YES
+            else 0,
+            "you": {"income": {"self_employed": self.is_self_employed.data}},
+            # TEST DATA
+            "property_set": [],
+            "category": "debt",
+            "on_passported_benefits": "0",
+            "on_nass_benefits": "0",
+            "specific_benefits": {},
+        }
+
+        payload["you"] = {
+            "savings": {
+                "credit_balance": 0,
+                "investment_balance": 0,
+                "asset_balance": 0,
+                "bank_balance": 0,
+            },
+            "deductions": {
+                "income_tax": {"per_interval_value": 0, "interval_period": "per_month"},
+                "mortgage": {"per_interval_value": 0, "interval_period": "per_month"},
+                "childcare": {"per_interval_value": 0, "interval_period": "per_month"},
+                "rent": {"per_interval_value": 0, "interval_period": "per_week"},
+                "maintenance": {"per_interval_value": 0, "interval_period": "per_week"},
+                "criminal_legalaid_contributions": 0,
+                "national_insurance": {
+                    "per_interval_value": 0,
+                    "interval_period": "per_month",
+                },
+            },
+            "income": {
+                "self_employment_drawings": {
+                    "per_interval_value": 0,
+                    "interval_period": "per_month",
+                },
+                "benefits": {"per_interval_value": 0, "interval_period": "per_month"},
+                "maintenance_received": {
+                    "per_interval_value": 0,
+                    "interval_period": "per_week",
+                },
+                "self_employed": "0",
+                "tax_credits": {
+                    "per_interval_value": 0,
+                    "interval_period": "per_month",
+                },
+                "earnings": {"per_interval_value": 0, "interval_period": "per_month"},
+                "child_benefits": {
+                    "per_interval_value": 0,
+                    "interval_period": "per_month",
+                },
+                "other_income": {
+                    "per_interval_value": 0,
+                    "interval_period": "per_month",
+                },
+                "pension": {"per_interval_value": 0, "interval_period": "per_month"},
+                "total": 0,
+            },
+        }
+
+        if payload["has_partner"] and self.partner_is_self_employed.data == YES:
+            payload["partner"] = {
+                "income": {"self_employed": self.partner_is_self_employed.data}
+            }
+
+        if self.own_property.data:
+            # TODO: Get property data
+            pass
+
+        if self.have_savings or self.have_valuables:
+            # TODO: Get savings data
+            pass
+
+        if self.on_benefits.data:
+            # TODO: Get benefits data
+            pass
+
+        # TODO: Get income and outgoing data
+
+        return payload
+
+    """
+{
+   "on_passported_benefits":"0",
+   "category":"u""debt",
+   "is_you_or_your_partner_over_60":"u""0",
+   "has_partner":"0",
+   "property_set":[
+      
+   ],
+   "specific_benefits":{
+      
+   },
+   "dependants_old":0,
+   "you":{
+      "savings":{
+         "credit_balance":0,
+         "investment_balance":0,
+         "asset_balance":0,
+         "bank_balance":0
+      },
+      "deductions":{
+         "income_tax":{
+            "per_interval_value":0,
+            "interval_period":"per_month"
+         },
+         "mortgage":{
+            "per_interval_value":0,
+            "interval_period":"per_month"
+         },
+         "childcare":{
+            "per_interval_value":0,
+            "interval_period":"per_month"
+         },
+         "rent":{
+            "per_interval_value":0,
+            "interval_period":"u""per_week"
+         },
+         "maintenance":{
+            "per_interval_value":0,
+            "interval_period":"u""per_week"
+         },
+         "criminal_legalaid_contributions":0,
+         "national_insurance":{
+            "per_interval_value":0,
+            "interval_period":"per_month"
+         }
+      },
+      "income":{
+         "self_employment_drawings":{
+            "per_interval_value":0,
+            "interval_period":"per_month"
+         },
+         "benefits":{
+            "per_interval_value":0,
+            "interval_period":"per_month"
+         },
+         "maintenance_received":{
+            "per_interval_value":0,
+            "interval_period":"u""per_week"
+         },
+         "self_employed":"0",
+         "tax_credits":{
+            "per_interval_value":0,
+            "interval_period":"per_month"
+         },
+         "earnings":{
+            "per_interval_value":0,
+            "interval_period":"per_month"
+         },
+         "child_benefits":{
+            "per_interval_value":0,
+            "interval_period":"per_month"
+         },
+         "other_income":{
+            "per_interval_value":0,
+            "interval_period":"u""per_week"
+         },
+         "pension":{
+            "per_interval_value":0,
+            "interval_period":"u""per_week"
+         }
+      }
+   },
+   "dependants_young":0,
+   "on_nass_benefits":"0"
+}
+    """
