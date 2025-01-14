3b156418cc683552dae21f9abc9c11fe4b9adad1
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-01-14 17:53:45 +0000

    WIP add property page

--- app/means_test/routes.py
+++ app/means_test/routes.py
@@ -1,6 +1,14 @@
+from flask import render_template, request
 from app.means_test import bp
+from app.means_test.forms.property import PropertyForm
 
 
 @bp.get("/mean-test-review")
 def review():
     return "I am a holding page"
+
+
+@bp.route("/property", methods=["GET", "POST"])
+def property():
+    form = PropertyForm(data=request.form)
+    return render_template("means_test/property.html", form=form)
