0a28dec36d0d8ff751da74fc4f4ebf0212bd7746
Ben Millar <ben.millar@digital.justice.gov.uk>
2025-01-14 17:34:18 +0000

    Add is_eligible endpoint

--- app/means_test/api.py
+++ app/means_test/api.py
@@ -20,3 +20,9 @@ def update_means_test(payload):
         session["name"] = "Ben"
         print(session)
         return redirect(url_for(("categories.results.in_scope")))
+
+
+def is_eligible(reference):
+    means_test_endpoint = "checker/api/v1/eligibility_check/"
+    response = cla_backend.post(f"{means_test_endpoint}{reference}/is_eligible/")
+    return response["is_eligible"]
