fffcaac289df349f1c6989029f584919ff8b39f0
said-moj <45761276+said-moj@users.noreply.github.com>
2025-01-17 10:44:56 +0000

    Move MoneyField into the fields module (#99)
    * Move MoneyField into the fields module
    * Add unit tests for MoneyField

--- app/means_test/forms/money.py
+++ app/means_test/forms/money.py
@@ -1,84 +1,17 @@
-from flask import render_template
 from wtforms.fields.simple import SubmitField
-from wtforms.validators import InputRequired
-from wtforms.widgets import TextInput
-from markupsafe import Markup
-from wtforms import Field
 from app.means_test.forms import BaseMeansTestForm
 from app.means_test.validators import MoneyIntervalAmountRequired, ValidateIfSession
 from flask_babel import lazy_gettext as _
-
-
-class CombinedTextWidget(TextInput):
-    def __call__(self, field, *args, **kwargs):
-        # Pass the current values to the template
-        return Markup(
-            render_template(
-                "means_test/components/money-field.html",
-                field=field,
-                value=field.value,
-                interval=field.interval,
-            )
-        )
-
-
-class CombinedTextField(Field):
-    """
-    A field that handles two separate text inputs.
-    The raw data will contain both values separately.
-    """
-
-    validators = [InputRequired("Test")]
-
-    def __init__(self, label=None, hint_text=None, validators=None, **kwargs):
-        super().__init__(label, validators, **kwargs)
-        self.title = label
-        self.hint = hint_text
-        self.value = None  # Amount
-        self.interval = None  # Frequency
-        self.field_with_error = []
-
-    def process_formdata(self, valuelist):
-        """Process the form data from both inputs"""
-        if valuelist and len(valuelist) >= 2:
-            # Handle the data coming from the form fields named field.id[value] and field.id[interval]
-            self.value = valuelist[0]
-            self.interval = valuelist[1]
-
-    def _value(self):
-        if self.raw_data:
-            return " ".join(self.raw_data)
-        else:
-            return self.data and self.data.strftime(self.format) or ""
-
-    # def validate(self, form, extra_validators=None):
-    #     """Validate both inputs"""
-    #     self.errors = []
-    #     self.field_with_error = []
-    #     if not self.value and not self.interval:
-    #         self.errors.append(
-    #             "Enter the total amount of maintenance you receive, or 0 if this doesn’t apply to you"
-    #         )
-    #         self.field_with_error = ["value", "interval"]
-    #         return False
-    #     if not self.value:
-    #         self.errors.append("Amount is required")
-    #         self.field_with_error = ["value"]
-    #         return False
-    #     if not self.interval:
-    #         self.errors.append("Frequency is required")
-    #         self.field_with_error = ["interval"]
-    #         return False
-    #     return True
+from app.means_test.fields import MoneyField, MoneyFieldWidgetWidget
 
 
 class ExampleForm(BaseMeansTestForm):
     template = "means_test/form-page.html"
 
-    question = CombinedTextField(
+    question = MoneyField(
         "Test money field",
         hint_text="Hint text",
-        widget=CombinedTextWidget(),
+        widget=MoneyFieldWidgetWidget(),
         validators=[
             MoneyIntervalAmountRequired(
                 message=_(
@@ -92,10 +25,11 @@ class ExampleForm(BaseMeansTestForm):
         ],
     )
 
-    partner_question = CombinedTextField(
+    partner_question = MoneyField(
         "Test money field",
         hint_text="Hint text",
-        widget=CombinedTextWidget(),
+        exclude_intervals=["per_year"],
+        widget=MoneyFieldWidgetWidget(),
         validators=[
             ValidateIfSession("has_partner", True),
             MoneyIntervalAmountRequired(
