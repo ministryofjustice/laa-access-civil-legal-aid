dba1bebc24b1993c49c3e3887f4dd9bfb1e35f82
said-moj <45761276+said-moj@users.noreply.github.com>
2025-01-17 10:44:56 +0000

    Move MoneyField into the fields module (#99)
    * Move MoneyField into the fields module
    * Add unit tests for MoneyField

--- app/means_test/fields.py
+++ app/means_test/fields.py
@@ -53,6 +53,8 @@ class MoneyField(Field):
         **kwargs,
     ):
         super().__init__(label, validators, **kwargs)
+        print(exclude_intervals)
+        print(kwargs)
         self.title = label
         self.hint = hint_text
         self.value = None  # Amount
