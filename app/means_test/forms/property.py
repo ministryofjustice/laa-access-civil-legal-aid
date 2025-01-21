09f6a2c31390950c5ec61e9df7d92c58f5ce8177
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-01-21 15:58:36 +0000

    Update to the partner fields

--- app/means_test/forms/property.py
+++ app/means_test/forms/property.py
@@ -1,5 +1,6 @@
 from flask import session
 from wtforms.fields import RadioField, IntegerField
+from wtforms.fields.core import Label
 from govuk_frontend_wtf.wtforms_widgets import GovTextInput
 from wtforms.validators import InputRequired, NumberRange
 from app.means_test.widgets import MeansTestRadioInput
@@ -8,10 +9,37 @@ from app.means_test import YES
 from app.means_test.forms import BaseMeansTestForm
 
 
+class PartnerRadioField(RadioField):
+    def __init__(self, label, partner_label, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._label = label
+        self._partner_label = partner_label
+
+    @property
+    def label(self):
+        if session.get_eligibility().has_partner:
+            return Label(self.name, self._partner_label)
+        else:
+            return Label(self.name, self._label)
+
+    @label.setter
+    def label(self, value):
+        pass
+
+
 class PropertyForm(BaseMeansTestForm):
-    title = _("Your property")
+    @property
+    def title(self):
+        if session.get_eligibility().has_partner:
+            return _("You and your partner’s property")
+        else:
+            return _("Your property")
 
     template = "means_test/property.html"
+
+    @property
+    def has_partner(self):
+        return session.get_eligibility().has_partner
 
     @classmethod
     def should_show(cls) -> bool:
@@ -28,8 +56,11 @@ class PropertyForm(BaseMeansTestForm):
         validators=[InputRequired(message=_("Tell us whether this is your main home"))],
     )
 
-    other_shareholders = RadioField(
-        "Does anyone else own a share of the property?",
+    other_shareholders = PartnerRadioField(
+        partner_label=_(
+            "Does anyone else (other than you or your partner) own a share of the property?"
+        ),
+        label=_("Does anyone else own a share of the property?"),
         choices=[("yes", "Yes"), ("no", "No")],
         widget=MeansTestRadioInput(),
         description="Select ‘Yes’ if you share ownership with a friend, relative or ex-partner",
@@ -65,11 +96,10 @@ class PropertyForm(BaseMeansTestForm):
     )
 
     mortgage_payments = IntegerField(
-        "How much is left to pay on the mortgage?",
+        "How much was your monthly mortgage repayment last month?",
         widget=GovTextInput(),
-        description="Include the full amount owed, even if the property has shared ownership, or enter 0 if you have no mortgage",
         validators=[
-            InputRequired(message=_("Tell us how much is left to pay on the mortgage")),
+            InputRequired(message=_("Enter your mortgage repayment for last month")),
             NumberRange(
                 min=0, max=999999999, message=_("Enter a value of more than £0")
             ),
