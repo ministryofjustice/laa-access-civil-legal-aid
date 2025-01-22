714d451076cea220ad3d7fa075eac1b9ad980699
said-moj <45761276+said-moj@users.noreply.github.com>
2025-01-22 09:16:09 +0000

    Add new MoneyInterval helper class.
    Make MoneyField.data return the MoneyInterval.to_json because flask will not be able to serialize a MoneyInterval object
    This means that MoneyInterval.to_json can be used as payload to send to the backend for any MoneyField fields

--- app/means_test/fields.py
+++ app/means_test/fields.py
@@ -3,6 +3,7 @@ from wtforms.widgets import TextInput
 from markupsafe import Markup
 from wtforms import Field
 from flask_babel import lazy_gettext as _
+from app.means_test.money_interval import MoneyInterval
 
 
 class MoneyFieldWidgetWidget(TextInput):
@@ -44,6 +45,14 @@ class MoneyField(Field):
             )
         return choices
 
+    @property
+    def interval(self):
+        return self.data["interval_period"]
+
+    @property
+    def value(self):
+        return self.data["per_interval_value_pounds"]
+
     def __init__(
         self,
         label=None,
@@ -55,30 +64,31 @@ class MoneyField(Field):
         super().__init__(label, validators, **kwargs)
         self.title = label
         self.hint = hint_text
-        self.value = None  # Amount
-        self.interval = None  # Frequency
         self.field_with_error = []
         self._intervals = self._intervals.copy()
         if exclude_intervals:
             for interval in exclude_intervals:
                 del self._intervals[interval]
 
+    @property
+    def data(self):
+        if self._data is None:
+            instance = MoneyInterval()
+        elif isinstance(self._data, MoneyInterval):
+            instance = self._data
+        else:
+            instance = MoneyInterval(self._data)
+        return instance.to_json()
+
+    @data.setter
+    def data(self, data):
+        self._data = data
+
     def process_formdata(self, valuelist):
         """Process the form data from both inputs"""
         if valuelist and len(valuelist) == 2:
             # Handle the data coming from the form fields named field.id[value] and field.id[interval]
-            self.value = valuelist[0]
-            self.interval = valuelist[1]
-
-    def validate(self, form, extra_validators=None):
-        if self.interval not in self._intervals:
-            raise ValueError(
-                f"Invalid {self.interval} interval value given for field {self.name}"
-            )
-        return super().validate(form, extra_validators)
-
-    def _value(self):
-        if self.raw_data:
-            return " ".join(self.raw_data)
-        else:
-            return self.data and self.data.strftime(self.format) or ""
+            self.data = valuelist
+        elif valuelist and len(valuelist) == 1 and isinstance(valuelist[0], dict):
+            # Data being restored from the session
+            self.data = valuelist[0]
