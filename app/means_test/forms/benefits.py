b4c7224f2b5ff8aa34f147c71df3467f605deb13
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-01-31 11:43:53 +0000

    move benefits data

--- app/means_test/forms/benefits.py
+++ app/means_test/forms/benefits.py
@@ -11,8 +11,50 @@ from app.means_test.validators import (
     ValidateIf,
     ValidateIfType,
 )
-from app.means_test.data import BenefitsData
 from app.means_test import YES, NO
+
+from dataclasses import dataclass, field
+
+
+@dataclass
+class BenefitsData:
+    BENEFITS_CHOICES = [
+        ("child_benefit", _("Child Benefit")),
+        ("pension_credit", _("Guarantee Credit")),
+        ("income_support", _("Income Support")),
+        ("job_seekers_allowance", _("Income-based Jobseeker's Allowance")),
+        ("employment_support", _("Income-related Employment and Support Allowance")),
+        ("universal_credit", _("Universal Credit")),
+        ("other-benefit", _("Any other benefits")),
+    ]
+    benefits: list = field(default_factory=list)
+    child_benefits: dict = field(default_factory=dict)
+
+    @property
+    def passported_benefits(self):
+        others = ["child_benefit", "other-benefit"]
+        return [name for name, label in self.BENEFITS_CHOICES if name not in others]
+
+    @property
+    def specific_benefits(self):
+        return {
+            "pension_credit": "pension_credit" in self.benefits,
+            "job_seekers_allowance": "job_seekers_allowance" in self.benefits,
+            "employment_support": "employment_support" in self.benefits,
+            "universal_credit": "universal_credit" in self.benefits,
+            "income_support": "income_support" in self.benefits,
+        }
+
+    @property
+    def is_passported(self) -> bool:
+        return bool(set(self.benefits).intersection(self.passported_benefits))
+
+
+@dataclass
+class AdditionalBenefitData:
+    benefits: list = field(default_factory=list)
+    other_benefits: list = field(default_factory=list)
+    total_other_benefit: dict = field(default_factory=dict)
 
 
 def get_benefits_choices():
