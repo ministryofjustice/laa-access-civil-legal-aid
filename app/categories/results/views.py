b28396754a8d3d26c05fe8a393ea8d52c035d703
Ben Millar <ben.millar@digital.justice.gov.uk>
2025-02-05 16:45:02 +0000

    Add refer page

--- app/categories/results/views.py
+++ app/categories/results/views.py
@@ -1,3 +1,4 @@
+from app.categories.constants import Category
 from app.categories.views import CategoryPage
 from flask import session, redirect, url_for, render_template
 from app.find_a_legal_adviser.laalaa import get_category_code as get_fala_category_code
@@ -14,14 +15,16 @@ class ResultPage(CategoryPage):
     @staticmethod
     def get_context():
         category = session.category
-        alt_help_category = category.article_category_name
-        organisations = cla_backend.get_help_organisations(alt_help_category)
+        article_category_name = (
+            category.article_category_name
+            if isinstance(category, Category)
+            else "other"
+        )
+        organisations = cla_backend.get_help_organisations(article_category_name)
         return {
-            "category_name": alt_help_category,
+            "category_name": article_category_name,
             "organisations": organisations,
-            "fala_category_code": get_fala_category_code(
-                category.article_category_name
-            ),
+            "fala_category_code": get_fala_category_code(article_category_name),
         }
 
     def dispatch_request(self):
