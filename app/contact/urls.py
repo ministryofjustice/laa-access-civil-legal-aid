201cb85fbba4b68653dd4435d22b994508a123c4
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-02-06 14:02:53 +0000

    Initial commit

--- app/contact/urls.py
+++ app/contact/urls.py
@@ -1,16 +1,10 @@
 from app.contact import bp
-from app.categories.views import CategoryPage
-from app.contact.forms import ReasonsForContactingForm
+from app.contact.forms import ReasonsForContactingForm, ContactUsForm
 from app.api import cla_backend
 from flask import request, redirect, url_for, render_template
 import logging
 
 logger = logging.getLogger(__name__)
-
-bp.add_url_rule(
-    "/contact",
-    view_func=CategoryPage.as_view("contact_us", template="contact/contact.html"),
-)
 
 
 @bp.route("/reasons-for-contacting", methods=["GET", "POST"])
@@ -24,3 +18,9 @@ def reasons_for_contacting():
         logger.info("API Response: %s", result)
         return redirect(url_for(next_step))
     return render_template("contact/rfc.html", form=form)
+
+
+@bp.route("/contact-us", methods=["GET", "POST"])
+def contact_us():
+    form = ContactUsForm()
+    return render_template("contact/contact.html", form=form)
