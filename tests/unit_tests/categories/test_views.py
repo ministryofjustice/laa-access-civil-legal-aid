8510dedb31236a9bbab64f1dde55da4bbee0b275
said-moj <45761276+said-moj@users.noreply.github.com>
2025-02-20 13:52:30 +0000

    Add unit tests for QuestionPage.get_next_page

--- tests/unit_tests/categories/test_views.py
+++ tests/unit_tests/categories/test_views.py
@@ -7,6 +7,10 @@ from app.categories.results.views import (
     CannotFindYourProblemPage,
     NextStepsPage,
 )
+from wtforms import RadioField
+from wtforms.validators import InputRequired
+from app.categories.widgets import CategoryRadioInput
+from app.categories.views import QuestionPage, QuestionForm
 
 
 def test_category_page_dispatch(app):
@@ -69,3 +73,48 @@ class TestNextStepsPage:
     def test_init_with_no_help_organisations(self):
         page = NextStepsPage(get_help_organisations=False)
         assert page.template == "categories/next-steps.html"
+
+
+def test_question_page_next_page(app):
+    class TestQuestionForm(QuestionForm):
+        next_step_mapping = {
+            "yes": "categories.results.in_scope",
+            "no": "categories.results.refer",
+            "notsure": "categories.index",
+            "fala": {
+                "endpoint": "find-a-legal-adviser.search",
+                "category": "mhe",
+                "secondary_category": "com",
+            },
+        }
+        question = RadioField(
+            "This is a test question?",
+            widget=CategoryRadioInput(
+                show_divider=False
+            ),  # Uses our override class to support setting custom CSS on the label title
+            validators=[InputRequired(message="Validation failed message")],
+            choices=[
+                ("yes", "Yes"),
+                ("no", "No"),
+            ],
+        )
+
+    with app.app_context():
+        form = TestQuestionForm(category=FAMILY, question="yes")
+        view = QuestionPage(form_class=form)
+        assert "/legal-aid-available" == view.get_next_page("yes")
+
+        form = TestQuestionForm(category=FAMILY, question="no")
+        view = QuestionPage(form_class=form)
+        assert "/refer" == view.get_next_page("no")
+
+        form = TestQuestionForm(category=FAMILY, question="notsure")
+        view = QuestionPage(form_class=form)
+        assert "/find-your-problem" == view.get_next_page("notsure")
+
+        form = TestQuestionForm(category=FAMILY, question="notsure")
+        view = QuestionPage(form_class=form)
+        assert (
+            "/find-a-legal-adviser?category=mhe&secondary_category=com"
+            == view.get_next_page("fala")
+        )
