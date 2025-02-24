691c59297937e6854c3eb7c90bcee76dfe1515e9
Ben Millar <ben.millar@digital.justice.gov.uk>
2025-02-24 01:16:59 +0000

    Add additional are you at risk of harm routes

--- tests/functional_tests/categories/test_more_problems.py
+++ tests/functional_tests/categories/test_more_problems.py
@@ -1,5 +1,5 @@
 import pytest
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
 FALA_ROUTES = [
     # Link text, FALA Primary Category, FALA Secondary Category
@@ -19,6 +19,12 @@ FALA_ROUTES = [
     ("Trafficking, modern slavery", "immas", ""),
 ]
 
+DOMESTIC_ABUSE_ROUTES = [
+    "Domestic abuse - if you have been accused",
+    "Female genital mutilation (FGM)",
+    "Forced marriage",
+]
+
 
 @pytest.mark.usefixtures("live_server")
 class TestMoreProblems:
@@ -28,8 +34,17 @@ class TestMoreProblems:
     ):
         page.get_by_role("link", name="More problems covered by legal aid").click()
         page.get_by_role("link", name=route).click()
-        page.get_by_role("heading", name="Find a legal adviser").is_visible()
+        expect(page.get_by_role("heading", name="Find a legal adviser")).to_be_visible()
         if primary_category:
             assert f"category={primary_category}" in page.url
         if secondary_category:
             assert f"secondary_category={secondary_category}" in page.url
+
+    @pytest.mark.parametrize("route", DOMESTIC_ABUSE_ROUTES)
+    def test_domestic_abuse_routes(self, page: Page, route: str):
+        """These pages route to the "Are you worried about someone's safety?" page in the domestic abuse journey."""
+        page.get_by_role("link", name="More problems covered by legal aid").click()
+        page.get_by_role("link", name=route).click()
+        expect(
+            page.get_by_role("heading", name="Are you worried about someone's safety?")
+        ).to_be_visible()
