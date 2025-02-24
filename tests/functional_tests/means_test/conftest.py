1a36dd11bb1fc0940c4d39dfd3590e3e7f09a074
said-moj <45761276+said-moj@users.noreply.github.com>
2025-02-24 15:55:26 +0000

    Add more functional tests for check your answers page

--- tests/functional_tests/means_test/conftest.py
+++ tests/functional_tests/means_test/conftest.py
@@ -16,14 +16,14 @@ def complete_about_you_form(
 ):
     for question, answer in about_you_answers.items():
         form_group = page.get_by_role("group", name=question)
-        if question == "Do you have a partner":
+        if question == "Do you have a partner?":
             locator = "#has_partner" if answer == "Yes" else "#has_partner-2"
             form_group.locator(locator).check()
-            continue
-        if question == "How many children aged 15 or under?":
+        elif question == "How many children aged 15 or under?":
             page.locator("#num_children").fill(answer)
-            continue
-        form_group.get_by_label(answer).first.check()
+        else:
+            form_group.get_by_label(answer).first.check()
+
     # Submit form
     page.get_by_role("button", name="Continue").click()
     return page
@@ -45,3 +45,29 @@ def complete_benefits_form(page: Page, benefits_answers: dict, complete_about_yo
     page.get_by_role("button", name="Continue").click()
 
     return page
+
+
+def assert_about_you_form_is_prefilled(page: Page, about_you_answers: dict):
+    for question, answer in about_you_answers.items():
+        if question == "Do you have a partner?":
+            selector = "#has_partner" if answer == "Yes" else "#has_partner-2"
+            expect(page.locator(selector)).to_be_checked()
+        elif question == "How many children aged 15 or under?":
+            expect(page.locator("#num_children")).to_have_value(answer)
+        else:
+            locator = page.get_by_text(question).locator("..")
+            expect(locator.get_by_label(answer)).to_be_checked()
+
+
+def assert_benefits_form_is_prefilled(page: Page, benefits_answers: dict):
+    for question, answer in benefits_answers.items():
+        if question == "If yes, enter the total amount you get for all your children":
+            expect(page.get_by_label("Amount")).to_have_value(answer["Amount"])
+            expect(
+                page.get_by_label("Frequency").locator("option:checked")
+            ).to_have_text(answer["Frequency"])
+        elif isinstance(answer, list):
+            for label in answer:
+                expect(page.get_by_label(label)).to_be_checked()
+        else:
+            expect(page.get_by_label(question)).to_be_checked()
