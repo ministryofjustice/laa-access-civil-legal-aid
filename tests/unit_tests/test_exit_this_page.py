d25b8493b7bc960b861a80e4fdebc1addc43e755
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-03-10 11:40:23 +0000

    Update tests

--- tests/unit_tests/test_exit_this_page.py
+++ tests/unit_tests/test_exit_this_page.py
@@ -3,24 +3,24 @@ from flask import session
 from app.main import inject_exit_this_page
 
 
+class Category(dict):
+    def __init__(self, exit_page, code="default_code"):
+        super().__init__()
+        self["exit_page"] = exit_page
+        self["code"] = code
+
+
 @pytest.mark.parametrize(
     "session_data, expected_output",
     [
         ({}, {"show_exit_this_page": False}),
-        ({"category": {}}, {"show_exit_this_page": False}),
-        ({"category": {"exit_page": True}}, {"show_exit_this_page": True}),
-        ({"category": {"exit_page": False}}, {"show_exit_this_page": False}),
-        (
-            {"category": type("Category", (object,), {"exit_page": True})()},
-            {"show_exit_this_page": True},
-        ),
-        (
-            {"category": type("Category", (object,), {"exit_page": False})()},
-            {"show_exit_this_page": False},
-        ),
+        ({"category": None}, {"show_exit_this_page": False}),
+        ({"category": Category(True, "domestic_abuse")}, {"show_exit_this_page": True}),
+        ({"category": Category(False, "housing")}, {"show_exit_this_page": False}),
     ],
 )
 def test_inject_exit_this_page(session_data, expected_output, app):
     with app.test_request_context():
+        session.clear()
         session.update(session_data)
         assert inject_exit_this_page() == expected_output
