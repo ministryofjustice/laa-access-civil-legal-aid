e3d24eaa7e7feca554a0fe29c15815d812f6ec76
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-03-21 09:43:45 +0000

    move to middleware

--- app/main/middleware.py
+++ app/main/middleware.py
@@ -6,3 +6,12 @@ def add_noindex_header(response):
     """Add a noindex header to all responses, to disallow search engines from indexing the page."""
     response.headers["X-Robots-Tag"] = "noindex"
     return response
+
+
+# Prevents the webbrowser from caching
+@bp.after_app_request
+def add_no_cache_headers(response):
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
--- tests/unit_tests/test_middleware.py
+++ tests/unit_tests/test_middleware.py
@@ -17,3 +17,15 @@ import pytest
 def test_noindex_header_on_routes(client, endpoint):
     response = client.get(endpoint)
     assert response.headers.get("X-Robots-Tag") == "noindex"
+
+
+def est_no_cache_headers(client):
+    response = client.get("/find-your-problem")
+
+    assert response.status_code == 200
+    assert (
+        response.headers.get("Cache-Control")
+        == "no-store, no-cache, must-revalidate, max-age=0"
+    )
+    assert response.headers.get("Pragma") == "no-cache"
+    assert response.headers.get("Expires") == "0"
