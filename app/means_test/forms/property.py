9a0b9113821e10bb5788d36677925ec9d5f022b4
said-moj <45761276+said-moj@users.noreply.github.com>
2025-03-21 13:53:18 +0000

    Property form should show only when user owns a property

--- app/means_test/forms/property.py
+++ app/means_test/forms/property.py
@@ -212,6 +212,9 @@ class PropertyForm(BaseMeansTestForm):
         ],
     )
 
+    def should_show(cls) -> bool:
+        return session.get_eligibility().owns_property
+
 
 class MultiplePropertiesForm(BaseMeansTestForm):
     title = _("Your property")
