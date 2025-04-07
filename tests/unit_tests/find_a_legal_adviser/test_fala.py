3ba011c89fdfcb91e3be57326407d35021a971f9
Ben Millar <ben.millar@digital.justice.gov.uk>
2025-04-07 17:09:34 +0100

    Update FALA unit tests

--- tests/unit_tests/find_a_legal_adviser/test_fala.py
+++ tests/unit_tests/find_a_legal_adviser/test_fala.py
@@ -1,28 +1,39 @@
 from unittest.mock import patch
 
+MOCK_LAALAA_RESULTS = {
+    "count": 1,
+    "results": [
+        {
+            "categories": ["COM"],
+            "organisation": {"name": "Mock Organisation"},
+            "location": {
+                "address": "Address",
+                "city": "",
+                "postcode": "SW1A 1AA",
+                "point": {"type": "Point", "coordinates": [1, 2]},
+                "type": "Outreach Office",
+            },
+            "distance": 10,
+        }
+    ],
+    "origin": {
+        "postcode": "SW1A 0AA",
+        "point": {"type": "Point", "coordinates": [1, 2]},
+    },
+}
 
-def test_result_page_with_single_category(app):
-    # Result page data
-    postcode = "SW1A 1AA"
-    category = "COM"
-    page_num = 1
-    postcode_region = "London"
 
-    # laalaa_search results
-    mock_results = {
-        "count": 1,
-        "results": [
-            {"categories": ["COM"], "organisation": {"name": "Mock Organisation"}}
-        ],
-    }
+class TestFindALegalAdviser:
+    def test_result_page_with_single_category(self, app, client):
+        postcode = "SW1A 1AA"
+        category = "COM"
+        page_num = 1
+        postcode_region = "London"
 
-    # Mocks laalaa_search
-    with patch(
-        "app.find_a_legal_adviser.laalaa.laalaa_search", return_value=mock_results
-    ) as mock_laalaa_search:
-        mock_laalaa_search(postcode=postcode, categories=[category], page=page_num)
-        # Mocks results page
-        with app.test_client() as client:
+        with patch(
+            "app.find_a_legal_adviser.routes.laalaa_search",
+            return_value=MOCK_LAALAA_RESULTS,
+        ) as mock_laalaa_search:
             response = client.get(
                 "/find-a-legal-adviser",
                 query_string={
@@ -33,43 +44,24 @@ def test_result_page_with_single_category(app):
                 },
             )
 
-        assert response.status_code == 200
-        assert response.request.path == "/find-a-legal-adviser"
-        mock_laalaa_search.assert_called_with(
-            postcode=postcode,
-            categories=[category],  # Verify the categories parameter
-            page=page_num,
-        )
+            assert response.status_code == 200
+            assert response.request.path == "/find-a-legal-adviser"
+            mock_laalaa_search.assert_called_with(
+                postcode=postcode,
+                categories=[category],
+                page=page_num,
+            )
 
+    def test_result_page_with_secondary_category(self, app, client):
+        postcode = "SW1A 1AA"
+        category = "COM"
+        secondary_category = "MHE"
+        page_num = 1
 
-def test_result_page_with_secondary_category(app):
-    # Result page data
-    postcode = "SW1A 1AA"
-    category = "COM"
-    secondary_category = "MHE"
-    page_num = 1
-    postcode_region = "London"
-
-    # laalaa_search results
-    mock_results = {
-        "count": 1,
-        "results": [
-            {
-                "categories": ["COM", "MHE"],
-                "organisation": {"name": "Mock Organisation"},
-            }
-        ],
-    }
-
-    # Mocks laalaa_search
-    with patch(
-        "app.find_a_legal_adviser.laalaa.laalaa_search", return_value=mock_results
-    ) as mock_laalaa_search:
-        mock_laalaa_search(
-            postcode=postcode, categories=[category, secondary_category], page=page_num
-        )
-        # Mocks results page
-        with app.test_client() as client:
+        with patch(
+            "app.find_a_legal_adviser.routes.laalaa_search",
+            return_value=MOCK_LAALAA_RESULTS,
+        ) as mock_laalaa_search:
             response = client.get(
                 "/find-a-legal-adviser",
                 query_string={
@@ -77,17 +69,62 @@ def test_result_page_with_secondary_category(app):
                     "secondary_category": secondary_category,
                     "postcode": postcode,
                     "page": page_num,
-                    "postcode_region": postcode_region,
                 },
             )
 
-        assert response.status_code == 200
-        assert response.request.path == "/find-a-legal-adviser"
-        mock_laalaa_search.assert_called_with(
-            postcode=postcode,
-            categories=[
-                category,
-                secondary_category,
-            ],  # Verify the categories parameter
-            page=page_num,
-        )
+            assert response.status_code == 200
+            assert response.request.path == "/find-a-legal-adviser"
+            mock_laalaa_search.assert_called_with(
+                postcode=postcode,
+                categories=[
+                    category,
+                    secondary_category,
+                ],
+                page=page_num,
+            )
+
+    @patch("app.find_a_legal_adviser.routes.render_template")
+    def test_result_page_with_no_category(self, app, client):
+        postcode = "SW1A 1AA"
+
+        with patch(
+            "app.find_a_legal_adviser.routes.laalaa_search",
+            return_value=MOCK_LAALAA_RESULTS,
+        ) as mock_laalaa_search:
+            client.get(
+                "/find-a-legal-adviser",
+                query_string={
+                    "postcode": postcode,
+                    "page": 1,
+                },
+            )
+
+            mock_laalaa_search.assert_called_with(
+                postcode=postcode,
+                categories=[],
+                page=1,
+            )
+
+    @patch("app.find_a_legal_adviser.routes.render_template")
+    def test_result_page_with_only_secondary_category(self, app, client):
+        postcode = "SW1A 1AA"
+        secondary_category = "MHE"
+
+        with patch(
+            "app.find_a_legal_adviser.routes.laalaa_search",
+            return_value=MOCK_LAALAA_RESULTS,
+        ) as mock_laalaa_search:
+            client.get(
+                "/find-a-legal-adviser",
+                query_string={
+                    "postcode": postcode,
+                    "secondary_category": secondary_category,
+                    "page": 1,
+                },
+            )
+
+            mock_laalaa_search.assert_called_with(
+                postcode=postcode,
+                categories=["MHE"],
+                page=1,
+            )
