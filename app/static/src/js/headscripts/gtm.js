c94675e62ff11d8d53a90068d5e73aa563333646
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-04-22 14:24:23 +0100

    Add clear first

--- app/static/src/js/headscripts/gtm.js
+++ app/static/src/js/headscripts/gtm.js
@@ -22,10 +22,11 @@ function add_GTM() {
 
     GTM_Loaded = true;
 }
-
+window.dataLayer = window.dataLayer || [];
 if (window.location.pathname.startsWith('/find-a-legal-adviser')) {
     window.dataLayer.push({
     'event': 'fala_search',
+    'category_name': 'test',
     });
 }
 // If user consents from banner then allow GTM to load
