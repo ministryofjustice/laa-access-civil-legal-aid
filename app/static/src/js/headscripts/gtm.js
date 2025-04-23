e9b1acbbaa0730403a51431b46a53d682db6e0a9
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-04-23 12:33:14 +0100

    Add wait for dom content to load

--- app/static/src/js/headscripts/gtm.js
+++ app/static/src/js/headscripts/gtm.js
@@ -24,14 +24,16 @@ function add_GTM() {
 }
 
 // Diagnosed
-if (window.location.pathname.endsWith('/legal-aid-available')) {
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push({
-        event: 'diagnosed',
-        category_code: window.sessionData.category_code,
-        category_name: window.sessionData.category_name
-    });
-}
+document.addEventListener('DOMContentLoaded', function () {
+    if (window.location.pathname.endsWith('/legal-aid-available')) {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            event: 'diagnosed',
+            category_code: window.sessionData.category_code,
+            category_name: window.sessionData.category_name
+        });
+    }
+})
 
 // Record mini FALA search
 document.addEventListener('click', function (e) {
