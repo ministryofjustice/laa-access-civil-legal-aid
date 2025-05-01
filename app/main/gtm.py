10661df699043aa3786f39e7034a573e48f3d4e6
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-05-01 09:22:50 +0100

    Change to get gtm anon, add return if no gtm anon

--- app/main/gtm.py
+++ app/main/gtm.py
@@ -39,10 +39,12 @@ def detect_gtm_anon_id():
 
     @after_this_request
     def remember_gtm_anon_id(response):
+        if not session.get("gtm_anon_id"):
+            return
         expiration_date = datetime.now(timezone.utc) + timedelta(days=730)
         response.set_cookie(
             "gtm_anon_id",
-            session["gtm_anon_id"],
+            session.get("gtm_anon_id"),
             expires=expiration_date,
         )
         return response
