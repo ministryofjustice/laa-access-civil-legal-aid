6e10f8ce2308514983d3987eb45fdf717a523f03
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-05-01 09:06:04 +0100

    Revert "Bug/LGA-3681 Repair GTM Anon ID" (#254)
    Revert "Bug/LGA-3681 Repair GTM Anon ID (#251)"
    
    This reverts commit ae8a134da13fb5cca33fe85b11d7d0bb6e1b1774.

--- app/main/gtm.py
+++ app/main/gtm.py
@@ -3,7 +3,6 @@ from app.main import bp
 from datetime import datetime, timedelta, timezone
 import uuid
 import re
-import json
 
 
 @bp.app_context_processor
@@ -22,27 +21,20 @@ def get_gtm_anon_id_from_cookie():
 
 @bp.before_app_request
 def detect_gtm_anon_id():
-    cookies_policy = request.cookies.get("cookies_policy", "{}")
-
-    try:
-        policy_dict = json.loads(cookies_policy)
-    except ValueError:
-        policy_dict = {}
-
-    if policy_dict.get("analytics") != "yes":
-        return
-
+    # gtm_anon_id is used to track user anonymously across services for Google Tag Manager.
     if "gtm_anon_id" in session:
         return
 
-    session["gtm_anon_id"] = str(uuid.uuid4())
+    anon_id_cookie = get_gtm_anon_id_from_cookie()
+    if anon_id_cookie:
+        session["gtm_anon_id"] = anon_id_cookie
+        return
 
     @after_this_request
     def remember_gtm_anon_id(response):
-        expiration_date = datetime.now(timezone.utc) + timedelta(days=730)
+        session["gtm_anon_id"] = str(uuid.uuid4())
+        expiration_date = datetime.now(timezone.utc) + timedelta(days=30)
         response.set_cookie(
-            "gtm_anon_id",
-            session["gtm_anon_id"],
-            expires=expiration_date,
+            "gtm_anon_id", session.get("gtm_anon_id"), expires=expiration_date
         )
         return response
