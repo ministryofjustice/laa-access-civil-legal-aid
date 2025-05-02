8571712d4cbabf0b3c56667904f105526af17a2c
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-05-02 08:38:43 +0100

    Bug/lga 3681 gtm anon (#255)
    * remove logic for gtm anon id
    
    * Remove from gtm session expired
    
    * Reset session expired gtm anon id
    
    * Add logging
    
    * Remove logging
    
    * Change to 2 years
    
    * Update expires
    
    * Change to 180 days
    
    * Remove print
    
    * Change to 2 years
    
    * Add string check
    
    * Update gtm anon to only show with agreed cookie policy:
    
    * Change to get gtm anon, add return if no gtm anon
    
    * Add gtm add anon event
    
    * Change name to push
    
    * Add gtm unit tests
    
    * Add functional tests
    
    * Amend gtm_anon logic
    
    * Add back to dom load
    
    * Add comment back in

--- app/main/gtm.py
+++ app/main/gtm.py
@@ -3,6 +3,7 @@ from app.main import bp
 from datetime import datetime, timedelta, timezone
 import uuid
 import re
+import json
 
 
 @bp.app_context_processor
@@ -22,6 +23,16 @@ def get_gtm_anon_id_from_cookie():
 @bp.before_app_request
 def detect_gtm_anon_id():
     # gtm_anon_id is used to track user anonymously across services for Google Tag Manager.
+    cookies_policy = request.cookies.get("cookies_policy", "{}")
+
+    try:
+        policy_dict = json.loads(cookies_policy)
+    except ValueError:
+        policy_dict = {}
+
+    if policy_dict.get("analytics") != "yes":
+        return
+
     if "gtm_anon_id" in session:
         return
 
@@ -30,11 +41,16 @@ def detect_gtm_anon_id():
         session["gtm_anon_id"] = anon_id_cookie
         return
 
+    session["gtm_anon_id"] = str(uuid.uuid4())
+
     @after_this_request
     def remember_gtm_anon_id(response):
-        session["gtm_anon_id"] = str(uuid.uuid4())
-        expiration_date = datetime.now(timezone.utc) + timedelta(days=30)
+        if not session.get("gtm_anon_id"):
+            return
+        expiration_date = datetime.now(timezone.utc) + timedelta(days=730)
         response.set_cookie(
-            "gtm_anon_id", session.get("gtm_anon_id"), expires=expiration_date
+            "gtm_anon_id",
+            session.get("gtm_anon_id"),
+            expires=expiration_date,
         )
         return response
