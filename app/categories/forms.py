0c458fce315a89b98bd3ec926259130a1591d6c6
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-05-20 09:02:37 +0100

    Change to more-info-required

--- app/categories/forms.py
+++ app/categories/forms.py
@@ -49,7 +49,10 @@ class SafeguardingQuestionForm(QuestionForm):
 
     next_step_mapping = {
         "yes": {"endpoint": "contact.contact_us_fast_tracked", "reason": "harm"},
-        "no": {"endpoint": "contact.contact_us_fast_tracked", "reason": "other"},
+        "no": {
+            "endpoint": "contact.contact_us_fast_tracked",
+            "reason": "more-info-required",
+        },
     }
 
     question = RadioField(
