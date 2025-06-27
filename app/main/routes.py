37cb35664275946ffaa6b4871bf7c2fcf04c814e
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-06-27 10:26:15 +0100

    LGA-3300 Fix Snyk Vulernabilities (#286)
    * Update routes vulnerabilities
    
    * Add comment

--- app/main/routes.py
+++ app/main/routes.py
@@ -89,14 +89,18 @@ def set_locale(locale):
         abort(404)
 
     if request.referrer:
-        parse = urlparse(request.referrer)
-        redirect_url = ["/", parse.path.strip("/")]
-        if parse.query:
-            redirect_url.append("?" + parse.query)
-    else:
-        redirect_url = ["/"]
+        parsed = urlparse(request.referrer)
+        if (
+            parsed.scheme in ("http", "https")
+            and parsed.netloc == request.host
+            and parsed.path.startswith("/")
+            and not parsed.path.startswith("//")  # Prevents protocol-relative URLs being used in redirects
+        ):
+            redirect_url = parsed.path
+        else:
+            redirect_url = url_for("main.start_page")
 
-    response = redirect("".join(redirect_url))
+    response = redirect(redirect_url)
     response = set_locale_cookie(response, locale)
     return response
 
@@ -188,7 +192,7 @@ def http_exception(error):
 @bp.app_errorhandler(CSRFError)
 def csrf_error(error):
     flash("The form you were submitting has expired. Please try again.")
-    return redirect(request.full_path)
+    return redirect(url_for("main.session_expired"))
 
 
 @bp.before_app_request
