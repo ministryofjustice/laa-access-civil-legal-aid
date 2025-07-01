cbcf59472950f9732422d71b0bbc1fc300e1aba9
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2025-07-01 13:08:49 +0100

    Add logic for no slots

--- app/contact/constants.py
+++ app/contact/constants.py
@@ -5,3 +5,9 @@ CONTACT_PREFERENCE = [
     ("callback", _("Call me back")),
     ("thirdparty", _("Call someone else instead of me")),
 ]
+
+# If there are no slots available for call me back this will show
+NO_SLOT_CONTACT_PREFERENCE = [
+    ("call", _("I will call Civil Legal Advice")),
+    ("thirdparty", _("I want Civil Legal Advice to call someone else instead of me")),
+]
--- app/contact/forms/choose_an_option.py
+++ app/contact/forms/choose_an_option.py
@@ -1,14 +1,25 @@
 from flask_babel import lazy_gettext as _
 from app.contact.forms import BaseForm
-from app.contact.constants import (
-    CONTACT_PREFERENCE,
-)
+from app.contact.constants import CONTACT_PREFERENCE, NO_SLOT_CONTACT_PREFERENCE
 from app.contact.widgets import ContactRadioInput
 from wtforms import RadioField
 from wtforms.validators import InputRequired
+from app.api import cla_backend
 
 
 class OptionForm(BaseForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Get the valid timeslots once from backend and cache them
+        self.time_slots = cla_backend.get_time_slots(num_days=8)
+        self.thirdparty_time_slots = cla_backend.get_time_slots(num_days=8, is_third_party_callback=True)
+        self._adjust_contact_options_for_availability()
+
+    def _adjust_contact_options_for_availability(self):
+        """Remove callback option if no slots are available"""
+        if len(self.time_slots) <= 1:
+            self.contact_type.choices = NO_SLOT_CONTACT_PREFERENCE
+
     title = _("Choose an option for your appointment")
     template = "contact/choose-an-option.html"
     url = "option-for-appointment"
