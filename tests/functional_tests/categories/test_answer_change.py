166b6506a76ae854fa976ee0af705aa9a5f545c1
joselopez-moj <jose.lopez@justice.gov.uk>
2026-08-20 15:47:17 +0100

    test: add missing assert for Inquest

--- tests/functional_tests/categories/test_answer_change.py
+++ tests/functional_tests/categories/test_answer_change.py
@@ -51,8 +51,11 @@ def test_bug_lga_3746_multiple_subcategories(page: Page):
 
 
 @pytest.mark.usefixtures("live_server")
-def test_inquest_category_redirects_to_find_a_legal_adviser(page: Page):
-    url = url_for("find-a-legal-adviser.search", _external=True)
-    assert url.endswith("/find-a-legal-adviser"), url
+def test_inquest_category_is_available_on_find_your_problem(page: Page):
+    url = url_for("categories.index", _external=True)
+    assert url.endswith("/find-your-problem"), url
     page.goto(url)
+
+    expect(page.get_by_role("link", name="Inquests for family members")).to_be_visible()
+    page.get_by_role("link", name="Inquests for family members").click()
     expect(page.get_by_role("heading", name="Legal aid covers this problem")).to_be_visible()
